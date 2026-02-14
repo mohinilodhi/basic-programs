b43e7ed722638e6b69b0c95f4834d6f7309d6343
mohini <lodhimohini5@gmail.com>
2026-02-14 20:37:29 +0530

    Day07: dictionary.py

---
+++ Day07/dictionary.py
@@ -0,0 +1,33 @@
+##Day07
+#dictinary
+print("welcome we are discuss here about the dictinary")
+
+#we store information about the student
+student1 = {
+    "name":"aakancha sharma",
+    "age":17,
+    "class":12,
+    "Sub":"PCM",
+    "marks":90
+}
+
+print(student1["name"])
+print(student1["age"])
+print(student1["class"])
+print(student1["marks"])
+
+student2 = {
+    "name":"anjli sharma",
+    "age":17,
+    "class":12,
+    "subject":"PCB",
+    "marks":90
+}
+print(student2["name"])
+print(student2["age"])
+print(student2["class"])
+print(student2["subject"])
+print(student2["marks"])
+
+#WE CAN STORE ANY TYPE OF INFORMATION IN THIS MANNER
+
